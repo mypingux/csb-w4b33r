7189badea269460b0d61fae39b3f792c9d49d44a
Code Sandbox <hello@codesandbox.io>
2022-03-09 10:04:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.02b4272d1046febfb158817887ad14f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beda15742aaf67196aa8f2d6bc5ccda1",
+    "url": "https://mypingux.github.io/csb-w4b33r/index.html"
+  },
+  {
+    "revision": "59299a42c3b7635831d3",
+    "url": "https://mypingux.github.io/csb-w4b33r/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04cd5a01fa79ba97ecc7",
+    "url": "https://mypingux.github.io/csb-w4b33r/static/js/2.526c9b78.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "https://mypingux.github.io/csb-w4b33r/static/js/2.526c9b78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59299a42c3b7635831d3",
+    "url": "https://mypingux.github.io/csb-w4b33r/static/js/main.a98fcec7.chunk.js"
+  },
+  {
+    "revision": "014cfbcaf6098dcc8717",
+    "url": "https://mypingux.github.io/csb-w4b33r/static/js/runtime-main.ea113e4e.js"
+  }
+]);
